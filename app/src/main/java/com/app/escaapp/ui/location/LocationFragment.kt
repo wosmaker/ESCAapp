d9c43c6bba6ece4fdcbdb42abaf8d6dc6165b34a
XirisXER <twice2541@gmail.com>
2019-11-19 21:21:47 +0700

    google map but API key didnt work so fuck it

--- app/src/main/java/com/app/escaapp/ui/location/LocationFragment.kt
+++ app/src/main/java/com/app/escaapp/ui/location/LocationFragment.kt
@@ -11,13 +11,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.app.escaapp.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.fragment_location.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class LocationFragment : Fragment() {
+class LocationFragment : Fragment() , OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,11 +39,28 @@ class LocationFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.trackLocation.text = "Gps Run"
-        getLocation(view)
+        var mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        if(mapFragment == null){
+            val fm = getFragmentManager()
+            val ft = fm?.beginTransaction()
+            mapFragment = SupportMapFragment.newInstance()
+            ft?.replace(R.id.map, mapFragment)?.commit()
+        }
+        mapFragment?.getMapAsync(this)
+        //view.trackLocation.text = "Gps Run"
+        //getLocation(view)
     }
 
-    private var location_service : LocationManager? = null
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    /*private var location_service : LocationManager? = null
     private  var locationGps : Location? = null
     private  var locationNetwork : Location? = null
 
@@ -96,7 +123,7 @@ class LocationFragment : Fragment() {
         }
         catch(ex: SecurityException) { }
 
-    }
+    }*/
 
 
 }
