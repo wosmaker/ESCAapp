f0d20bcac6bb08b367533cf6aa2cbb3fee38fb72
wos <wosmaker@gmaill.com>
2019-11-23 02:33:40 +0700

    upgrade :: db

--- app/src/main/java/com/app/escaapp/db/UsersDBHelper.kt
+++ app/src/main/java/com/app/escaapp/db/UsersDBHelper.kt
@@ -30,6 +30,10 @@ class UsersDBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,
         db.execSQL(Tb_user)
     }
 
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     fun addUser(user : UserModel):Boolean{
         val db = writableDatabase
         val values = ContentValues()
@@ -42,19 +46,13 @@ class UsersDBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,
         return (Integer.parseInt("$rowId") != -1)
     }
 
-    fun deleteUser(user : UserModel):Boolean{
+    fun deleteUser(userid : Int):Boolean{
         val db = writableDatabase
         val sel = "$id like ?"
-
-        val values = ContentValues()
-        values.put(id,user.id)
-        values.put(relate_name,user.relate_name)
-        values.put(phone_no,user.phone_no)
-        values.put(relation,user.relation)
-        val rowId = db.insert(table_name,null,values)
+        val selectArgs = arrayOf(userid.toString())
+        val rowId = db.delete(table_name,sel,selectArgs)
 
         return (Integer.parseInt("$rowId") != -1)
-
     }
 
     fun getUser(userid:Int): ArrayList<UserModel>{
@@ -75,13 +73,13 @@ class UsersDBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,
     }
 
     fun getAllUser():ArrayList<UserModel>{
-        val users = ArrayList<UserModel>
+        val users = ArrayList<UserModel>()
         val db = readableDatabase
         val cursor = db.rawQuery("select * from $table_name",null)
 
         if(cursor!!.moveToFirst()){
             do{
-                var _userId = cursor.getString(cursor.getColumnIndex(id))
+                var _userId = cursor.getInt(cursor.getColumnIndex(id))
                 var _name = cursor.getString(cursor.getColumnIndex(relate_name))
                 var _phone = cursor.getString(cursor.getColumnIndex(phone_no))
                 var _relation = cursor.getString(cursor.getColumnIndex(relation))
