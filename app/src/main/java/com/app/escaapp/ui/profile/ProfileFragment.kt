8b1867d8ebbb301dbe5a2e89c5e5d0c9da8866f1
wos <wosmaker@gmaill.com>
2019-11-23 22:42:19 +0700

    add :: popup

--- app/src/main/java/com/app/escaapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/app/escaapp/ui/profile/ProfileFragment.kt
@@ -1,36 +1,56 @@
 package com.app.escaapp.ui.profile
 
 import android.content.Context
-import android.content.Intent
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.graphics.component1
-import com.app.escaapp.FirstTime
+import android.widget.PopupWindow
+import android.widget.Toast
 import com.app.escaapp.R
-import com.app.escaapp.ui.location.LocationFragment
-import kotlinx.android.synthetic.main.activity_main_app.*
-import kotlinx.android.synthetic.main.activity_main_app.view.*
+import com.app.escaapp.ui.setting.SettingFragment
 import kotlinx.android.synthetic.main.fragment_profile.view.*
-import kotlinx.android.synthetic.main.navbar_botton.view.*
+import java.lang.Exception
 
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var popupView: View
+    private lateinit var popupWindow: PopupWindow
+
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
 
         val view = inflater!!.inflate(R.layout.fragment_profile, container, false)
-
+        popupView = LayoutInflater.from(activity).inflate(R.layout.fragment_setting,null)
+        popupWindow = PopupWindow(popupView,WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.MATCH_PARENT)
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val spName = "App_config"
+        val sp = activity!!.getSharedPreferences(spName,Context.MODE_PRIVATE)
 
+        try{
+
+            view.btn_popup.setOnClickListener {
+                /*  val intent = Intent(activity, FirstTime::class.java)
+                    startActivity(intent)*/
+                popupWindow.showAsDropDown(popupView)
+                SettingFragment().setting_begin(popupView,sp)
+                SettingFragment().setting_listen(popupView,sp)
+            }
+
+
+        }
+        catch (e:Exception ){
+            Toast.makeText(activity,"Error  : $e"  ,10000 ).show()
+        }
+
+        /*   popupView.inside.setOnClickListener {
+               popupWindow.dismiss()
+           }*/
     }
+
 
 
 }
