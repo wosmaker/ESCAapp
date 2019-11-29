24b35dd3dfa2dd6dfcaaf7e8e3257a94f4d8d9e3
wos <wosmaker@gmaill.com>
2019-11-29 21:50:34 +0700

    rename adapter

--- app/src/main/java/com/app/escaapp/ui/emergency/EmergencyFragment.kt
+++ app/src/main/java/com/app/escaapp/ui/emergency/EmergencyFragment.kt
@@ -1,13 +1,10 @@
 package com.app.escaapp.ui.emergency
 
 
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.telephony.PhoneNumberUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,15 +14,9 @@ import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import com.app.escaapp.NavBar
 import com.app.escaapp.R
-import com.app.escaapp.ui.manage.userAdapter
-import com.example.management.UserModel
 import com.example.management.UsersDBHelper
-import kotlinx.android.synthetic.main.fragment_call_list.view.*
-import kotlinx.android.synthetic.main.fragment_emergency.*
 import kotlinx.android.synthetic.main.fragment_emergency.view.*
-import kotlinx.android.synthetic.main.fragment_manage.view.*
 import kotlinx.android.synthetic.main.navbar_botton.view.*
-import java.text.NumberFormat
 
 /**
  * A simple [Fragment] subclass.
