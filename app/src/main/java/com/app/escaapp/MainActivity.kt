4afcda8047a89152d9e2e9e7de0e226a9b9d90d9
XirisXER <twice2541@gmail.com>
2019-11-30 08:10:38 +0700

    update :: location & interface to get current location
    for interface, look at myReceiver method

--- app/src/main/java/com/app/escaapp/MainActivity.kt
+++ app/src/main/java/com/app/escaapp/MainActivity.kt
@@ -1,26 +1,86 @@
 package com.app.escaapp
 
 import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.net.Uri
+import android.os.Bundle
+import android.os.IBinder
+
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.app.escaapp.ui.location.LocationUpdatesService
+import com.google.android.material.snackbar.Snackbar
+import android.content.BroadcastReceiver
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.content.IntentFilter
+import android.content.ServiceConnection
 import android.content.SharedPreferences
-import android.content.pm.PackageManager
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import com.app.escaapp.ui.location.Utils
+import androidx.preference.PreferenceManager
 import com.example.management.UserModel
 import com.example.management.UsersDBHelper
+
 import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java.simpleName
+
+    // Used in checking for runtime permissions.
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+
+    // The BroadcastReceiver used to listen from broadcasts from the service.
+    private lateinit var myReceiver: MyReceiver
+
+    // A reference to the service used to get location updates.
+    private var mService: LocationUpdatesService? = null
+
+    // Tracks the bound state of the service.
+    private var mBound = false
+
+    // UI elements.
+    private var mRequestLocationUpdatesButton: Button? = null
+    private var mRemoveLocationUpdatesButton: Button? = null
+
+    // Monitors the state of the connection to the service.
+    private val mServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            val binder = service as LocationUpdatesService.LocalBinder
+            mService = binder.getService()
+            mBound = true
+            mService?.requestLocationUpdates()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            mService = null
+            mBound = false
+        }
+    }
 
     val LocationPermissionRequestCode = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContentView(R.layout.fragment_emergency)
+        bindService( Intent(this, LocationUpdatesService::class.java), mServiceConnection,
+                Context.BIND_AUTO_CREATE)
+        // Check that the user hasn't revoked permissions by going to Settings.
+
+        if (Utils.requestingLocationUpdates(this)) {
+            if (!checkPermissions()) {
+                requestPermissions()
+            }
+        }
+        myReceiver = MyReceiver()
         val spName = "App_config"
         val sp:SharedPreferences = getSharedPreferences(spName, Context.MODE_PRIVATE)
         val editor:SharedPreferences.Editor = sp.edit()
@@ -43,8 +103,142 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, MainAppActivity::class.java)
             startActivity(intent)
         }
+
     }
 
+    override fun onStart() {
+        super.onStart()
+
+//        PreferenceManager.getDefaultSharedPreferences(this)
+//                .registerOnSharedPreferenceChangeListener(this)
+
+            if (!checkPermissions()) {
+                requestPermissions()
+            } else {
+                mService?.requestLocationUpdates()
+            }
+        LocalBroadcastManager.getInstance(this).registerReceiver(myReceiver,
+                IntentFilter(LocationUpdatesService.ACTION_BROADCAST))
+       // mRemoveLocationUpdatesButton.setOnClickListener(View.OnClickListener { mService.removeLocationUpdates() })
+        // Restore the state of the buttons when the activity (re)launches.
+        //setButtonsState(Utils.requestingLocationUpdates(this))
+        // Bind to the service. If the service is in foreground mode, this signals to the service
+// that since this activity is in the foreground, the service can exit foreground mode.
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        LocalBroadcastManager.getInstance(this).registerReceiver(myReceiver,
+                IntentFilter(LocationUpdatesService.ACTION_BROADCAST))
+    }
+
+//    override fun onPause() {
+//        LocalBroadcastManager.getInstance(this).unregisterReceiver(myReceiver)
+//        super.onPause()
+//    }
+
+    override fun onStop() {
+        if (mBound) { // Unbind from the service. This signals to the service that this activity is no longer
+// in the foreground, and the service can respond by promoting itself to a foreground
+// service.
+            unbindService(mServiceConnection)
+            mBound = false
+        }
+//        PreferenceManager.getDefaultSharedPreferences(this)
+//                .unregisterOnSharedPreferenceChangeListener(this)
+        super.onStop()
+    }
+
+    /**
+     * Returns the current state of the permissions needed.
+     */
+    private fun checkPermissions(): Boolean {
+        return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    private fun requestPermissions() {
+        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+        // Provide an additional rationale to the user. This would happen if the user denied the
+// request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.")
+            Snackbar.make(
+                    findViewById(R.id.master),
+                    R.string.permission_rationale,
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.ok) {
+                        // Request permission
+                        ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                                REQUEST_PERMISSIONS_REQUEST_CODE)
+                    }
+                    .show()
+        } else {
+            Log.i(TAG, "Requesting permission")
+            // Request permission. It's possible this can be auto answered if device policy
+// sets the permission in a given state or the user denied the permission
+// previously and checked "Never ask again".
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_PERMISSIONS_REQUEST_CODE)
+        }
+    }
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>,
+                                            grantResults: IntArray) {
+        Log.i(TAG, "onRequestPermissionResult")
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.size <= 0) { // If user interaction was interrupted, the permission request is cancelled and you
+// receive empty arrays.
+                Log.i(TAG, "User interaction was cancelled.")
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) { // Permission was granted.
+                mService!!.requestLocationUpdates()
+            } else { // Permission denied.
+               // setButtonsState(false)
+                Snackbar.make(
+                        findViewById(R.id.master),
+                        R.string.permission_denied_explanation,
+                        Snackbar.LENGTH_INDEFINITE)
+                        .setAction(R.string.settings) {
+                            // Build intent that displays the App settings screen.
+                            val intent = Intent()
+                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            val uri = Uri.fromParts("package",
+                                    BuildConfig.APPLICATION_ID, null)
+                            intent.data = uri
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            startActivity(intent)
+                        }
+                        .show()
+            }
+        }
+    }
+
+    /**
+     * Receiver for broadcasts sent by [LocationUpdatesService].
+     */
+    private class MyReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val location = intent?.getParcelableExtra<Location>(LocationUpdatesService.EXTRA_LOCATION)
+
+            if (location != null) {
+                Toast.makeText(context, Utils.getLocationText(location),
+                        Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+//    fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, s: String) { // Update the buttons state depending on whether location updates are being requested.
+//        if (s == Utils.KEY_REQUESTING_LOCATION_UPDATES) {
+//            setButtonsState(sharedPreferences.getBoolean(Utils.KEY_REQUESTING_LOCATION_UPDATES,
+//                    false))
+//        }
+//    }
+//
     fun initital(): ArrayList<UserModel> {
         val defaultCall = ArrayList<UserModel>()
         defaultCall.add(UserModel(0,"เหตุด่วน เหตุร้าย (เบอร์ฉุกเฉินแห่งชาติ)","911","",false))
---
+++ app/src/main/java/com/app/escaapp/ui/location/LocationUpdatesService.kt
@@ -0,0 +1,242 @@
+package com.app.escaapp.ui.location
+
+import android.app.*
+import android.app.NotificationManager.IMPORTANCE_HIGH
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
+import android.location.Location
+import android.os.*
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.app.escaapp.MainActivity
+import com.app.escaapp.R
+import com.google.android.gms.location.*
+
+class LocationUpdatesService : Service() {
+    private val mBinder: IBinder = LocalBinder()
+
+    private var mChangingConfiguration = false
+    private var mNotificationManager: NotificationManager? = null
+    /**
+     * Contains parameters used by [com.google.android.gms.location.FusedLocationProviderApi].
+     */
+    private var mLocationRequest: LocationRequest? = null
+    /**
+     * Provides access to the Fused Location Provider API.
+     */
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
+    /**
+     * Callback for changes in location.
+     */
+    private var mLocationCallback: LocationCallback? = null
+    private var mServiceHandler: Handler? = null
+    /**
+     * The current location.
+     */
+    private var mLocation: Location? = null
+    override fun onCreate() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mLocationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                onNewLocation(locationResult.lastLocation)
+            }
+        }
+        createLocationRequest()
+        getLastLocation()
+        val handlerThread = HandlerThread(TAG)
+        handlerThread.start()
+        mServiceHandler = Handler(handlerThread.looper)
+        mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        // Android O requires a Notification Channel.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name: CharSequence = getString(R.string.app_name)
+            // Create the channel for the notification
+            val mChannel = NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_DEFAULT)
+            // Set the Notification Channel for the Notification Manager.
+            mNotificationManager!!.createNotificationChannel(mChannel)
+        }
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        Log.i(TAG, "Service started")
+        val startedFromNotification = intent.getBooleanExtra(EXTRA_STARTED_FROM_NOTIFICATION,
+                false)
+
+        if (startedFromNotification) {
+            removeLocationUpdates()
+            stopSelf()
+        }
+        // Tells the system to not try to recreate the service after it has been killed.
+        return START_NOT_STICKY
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        mChangingConfiguration = true
+    }
+
+    override fun onBind(intent: Intent): IBinder? { // Called when a client comes to the foreground
+// and binds with this service. The service should cease to be a foreground service
+// when that happens.
+        Log.i(TAG, "in onBind()")
+        stopForeground(true)
+        mChangingConfiguration = false
+        return mBinder
+    }
+
+    override fun onRebind(intent: Intent) { // Called when a client returns to the foreground
+// and binds once again with this service. The service should cease to be a foreground
+// service when that happens.
+        Log.i(TAG, "in onRebind()")
+        stopForeground(true)
+        mChangingConfiguration = false
+        super.onRebind(intent)
+    }
+
+    override fun onUnbind(intent: Intent): Boolean {
+        Log.i(TAG, "Last client unbound from service")
+        // Called when the last client  unbinds from this
+// service. If this method is called due to a configuration change in MainActivity, we
+// do nothing. Otherwise, we make this service a foreground service.
+        if (!mChangingConfiguration && Utils.requestingLocationUpdates(this)) {
+            Log.i(TAG, "Starting foreground service")
+            startForeground(NOTIFICATION_ID, getNotification())
+        }
+        return true // Ensures onRebind() is called when a client re-binds.
+    }
+
+    override fun onDestroy() {
+        mServiceHandler!!.removeCallbacksAndMessages(null)
+    }
+
+    /**
+     * Makes a request for location updates. Note that in this sample we merely log the
+     * [SecurityException].
+     */
+    fun requestLocationUpdates() {
+        Log.i(TAG, "Requesting location updates")
+        Utils.setRequestingLocationUpdates(this, true)
+        startService(Intent(applicationContext, LocationUpdatesService::class.java))
+        try {
+            mFusedLocationClient!!.requestLocationUpdates(mLocationRequest,
+                    mLocationCallback, Looper.myLooper())
+        } catch (unlikely: SecurityException) {
+            Utils.setRequestingLocationUpdates(this, false)
+            Log.e(TAG, "Lost location permission. Could not request updates. $unlikely")
+        }
+    }
+
+    /**
+     * Removes location updates. Note that in this sample we merely log the
+     * [SecurityException].
+     */
+    fun removeLocationUpdates() {
+        Log.i(TAG, "Removing location updates")
+        try {
+            mFusedLocationClient!!.removeLocationUpdates(mLocationCallback)
+            Utils.setRequestingLocationUpdates(this, false)
+            stopSelf()
+        } catch (unlikely: SecurityException) {
+            Utils.setRequestingLocationUpdates(this, true)
+            Log.e(TAG, "Lost location permission. Could not remove updates. $unlikely")
+        }
+    }// Channel ID
+
+    // Set the Channel ID for Android O.
+    private fun getNotification(): Notification {
+        val intent = Intent(this, LocationUpdatesService::class.java)
+        val text: CharSequence = Utils.getLocationText(mLocation)
+        intent.putExtra(EXTRA_STARTED_FROM_NOTIFICATION, true)
+        val servicePendingIntent = PendingIntent.getService(this, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+        val activityPendingIntent = PendingIntent.getActivity(this, 0,
+                Intent(this, MainActivity::class.java), 0)
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .addAction(R.drawable.ic_launch, getString(R.string.launch_activity),
+                        activityPendingIntent)
+                .addAction(R.drawable.ic_cancel, getString(R.string.remove_location_updates),
+                        servicePendingIntent)
+                .setContentText(text)
+                .setContentTitle(Utils.getLocationTitle(this))
+                .setOngoing(true)
+                .setPriority(IMPORTANCE_HIGH)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker(text)
+                .setWhen(System.currentTimeMillis())
+        // Set the Channel ID for Android O.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId(CHANNEL_ID) // Channel ID
+        }
+        return builder.build()
+    }
+
+    private fun getLastLocation(){
+        try {
+            mFusedLocationClient!!.lastLocation
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful && task.result != null) {
+                            mLocation = task.result
+                        } else {
+                            Log.w(TAG, "Failed to get location.")
+                        }
+                    }
+        } catch (unlikely: SecurityException) {
+            Log.e(TAG, "Lost location permission.$unlikely")
+        }
+    }
+
+    private fun onNewLocation(location: Location) {
+        Log.i(TAG, "New location: $location")
+        mLocation = location
+        // Notify anyone listening for broadcasts about the new location.
+        val intent = Intent(ACTION_BROADCAST)
+        intent.putExtra(EXTRA_LOCATION, location)
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+        // Update notification content if running as a foreground service.
+
+
+    }
+
+    private fun createLocationRequest() {
+        mLocationRequest = LocationRequest()
+        mLocationRequest!!.interval = UPDATE_INTERVAL_IN_MILLISECONDS
+        mLocationRequest!!.fastestInterval = FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS
+        mLocationRequest!!.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+    inner class LocalBinder : Binder() {
+        fun getService(): LocationUpdatesService {
+            return this@LocationUpdatesService
+        }
+    }
+
+
+    companion object {
+        private const val PACKAGE_NAME = "com.app.escaapp"
+        private val TAG = LocationUpdatesService::class.java.simpleName
+        /**
+         * The name of the channel for notifications.
+         */
+        private const val CHANNEL_ID = "channel_01"
+        const val ACTION_BROADCAST = "$PACKAGE_NAME.broadcast"
+        const val EXTRA_LOCATION = "$PACKAGE_NAME.location"
+        private const val EXTRA_STARTED_FROM_NOTIFICATION = PACKAGE_NAME +
+                ".started_from_notification"
+        /**
+         * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+         */
+        private const val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 10000
+        /**
+         * The fastest rate for active location updates. Updates will never be more frequent
+         * than this value.
+         */
+        private const val FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2
+        /**
+         * The identifier for the notification displayed for the foreground service.
+         */
+        private const val NOTIFICATION_ID = 12345678
+    }
+}
