00f69ccc502509dd284f4a2e51933f2730a5d55b
XirisXER <twice2541@gmail.com>
2019-11-30 23:46:35 +0700

    update :: ui

--- app/src/main/java/com/app/escaapp/MainActivity.kt
+++ app/src/main/java/com/app/escaapp/MainActivity.kt
@@ -26,56 +26,56 @@ import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = MainActivity::class.java.simpleName
-
-    // Used in checking for runtime permissions.
-    private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
-
-    // The BroadcastReceiver used to listen from broadcasts from the service.
-    private lateinit var myReceiver: MyReceiver
-
-    // A reference to the service used to get location updates.
-    private var mService: LocationUpdatesService? = null
-
-    private var lastLocation: Location? = null
-    private lateinit var resultReceiver: AddressResultReceiver
-    // Tracks the bound state of the service.
-    private var mBound = false
-    private var first = true
-    // UI elements.
-    private var mRequestLocationUpdatesButton: Button? = null
-    private var mRemoveLocationUpdatesButton: Button? = null
-    private var fetchFinish = true
-    // Monitors the state of the connection to the service.
-    private val mServiceConnection = object : ServiceConnection {
-        override fun onServiceConnected(name: ComponentName, service: IBinder) {
-            val binder = service as LocationUpdatesService.LocalBinder
-            mService = binder.getService()
-            mBound = true
-            mService?.requestLocationUpdates()
-        }
-
-        override fun onServiceDisconnected(name: ComponentName) {
-            mService = null
-            mBound = false
-        }
-    }
+//    private val TAG = MainActivity::class.java.simpleName
+//
+//    // Used in checking for runtime permissions.
+//    private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+//
+//    // The BroadcastReceiver used to listen from broadcasts from the service.
+//    private lateinit var myReceiver: MyReceiver
+//
+//    // A reference to the service used to get location updates.
+//    private var mService: LocationUpdatesService? = null
+//
+//    private var lastLocation: Location? = null
+//    private lateinit var resultReceiver: AddressResultReceiver
+//    // Tracks the bound state of the service.
+//    private var mBound = false
+//    private var first = true
+//    // UI elements.
+//    private var mRequestLocationUpdatesButton: Button? = null
+//    private var mRemoveLocationUpdatesButton: Button? = null
+//    private var fetchFinish = true
+//    // Monitors the state of the connection to the service.
+//    private val mServiceConnection = object : ServiceConnection {
+//        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+//            val binder = service as LocationUpdatesService.LocalBinder
+//            mService = binder.getService()
+//            mBound = true
+//            mService?.requestLocationUpdates()
+//        }
+//
+//        override fun onServiceDisconnected(name: ComponentName) {
+//            mService = null
+//            mBound = false
+//        }
+//    }
 
     val LocationPermissionRequestCode = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bindService(Intent(this, LocationUpdatesService::class.java), mServiceConnection,
-                Context.BIND_AUTO_CREATE)
-        // Check that the user hasn't revoked permissions by going to Settings.
-
-        if (Utils.requestingLocationUpdates(this)) {
-            if (!checkPermissions()) {
-                requestPermissions()
-            }
-        }
-        myReceiver = MyReceiver()
-        resultReceiver = AddressResultReceiver(Handler())
+//        bindService(Intent(this, LocationUpdatesService::class.java), mServiceConnection,
+//        Context.BIND_AUTO_CREATE)
+//        // Check that the user hasn't revoked permissions by going to Settings.
+//
+//        if (Utils.requestingLocationUpdates(this)) {
+//            if (!checkPermissions()) {
+//                requestPermissions()
+//            }
+//        }
+//        myReceiver = MyReceiver()
+//        resultReceiver = AddressResultReceiver(Handler())
         val spName = "App_config"
         val sp: SharedPreferences = getSharedPreferences(spName, Context.MODE_PRIVATE)
         val editor: SharedPreferences.Editor = sp.edit()
@@ -108,13 +108,13 @@ class MainActivity : AppCompatActivity() {
 //        PreferenceManager.getDefaultSharedPreferences(this)
 //                .registerOnSharedPreferenceChangeListener(this)
 
-        if (!checkPermissions()) {
-            requestPermissions()
-        } else {
-            mService?.requestLocationUpdates()
-        }
-        LocalBroadcastManager.getInstance(this).registerReceiver(myReceiver,
-                IntentFilter(LocationUpdatesService.ACTION_BROADCAST))
+//        if (!checkPermissions()) {
+//            requestPermissions()
+//        } else {
+//            mService?.requestLocationUpdates()
+//        }
+//        LocalBroadcastManager.getInstance(this).registerReceiver(myReceiver,
+//                IntentFilter(LocationUpdatesService.ACTION_BROADCAST))
         // mRemoveLocationUpdatesButton.setOnClickListener(View.OnClickListener { mService.removeLocationUpdates() })
         // Restore the state of the buttons when the activity (re)launches.
         //setButtonsState(Utils.requestingLocationUpdates(this))
@@ -123,11 +123,11 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onResume() {
-        super.onResume()
-        LocalBroadcastManager.getInstance(this).registerReceiver(myReceiver,
-                IntentFilter(LocationUpdatesService.ACTION_BROADCAST))
-    }
+//    override fun onResume() {
+//        super.onResume()
+//        LocalBroadcastManager.getInstance(this).registerReceiver(myReceiver,
+//                IntentFilter(LocationUpdatesService.ACTION_BROADCAST))
+//    }
 
 //    override fun onPause() {
 //
@@ -136,142 +136,142 @@ class MainActivity : AppCompatActivity() {
 //        super.onPause()
 //    }
 
-    override fun onStop() {
-        if (mBound) { // Unbind from the service. This signals to the service that this activity is no longer
-// in the foreground, and the service can respond by promoting itself to a foreground
-// service.
-            unbindService(mServiceConnection)
-            mBound = false
-        }
-        //LocalBroadcastManager.getInstance(this).unregisterReceiver(myReceiver)
-//        PreferenceManager.getDefaultSharedPreferences(this)
-//                .unregisterOnSharedPreferenceChangeListener(this)
-        super.onStop()
-    }
+//    override fun onStop() {
+//        if (mBound) { // Unbind from the service. This signals to the service that this activity is no longer
+//// in the foreground, and the service can respond by promoting itself to a foreground
+//// service.
+//            unbindService(mServiceConnection)
+//            mBound = false
+//        }
+//        //LocalBroadcastManager.getInstance(this).unregisterReceiver(myReceiver)
+////        PreferenceManager.getDefaultSharedPreferences(this)
+////                .unregisterOnSharedPreferenceChangeListener(this)
+//        super.onStop()
+//    }
 
     /**
      * Returns the current state of the permissions needed.
      */
-    private fun checkPermissions(): Boolean {
-        return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-    }
-
-    private fun requestPermissions() {
-        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-        // Provide an additional rationale to the user. This would happen if the user denied the
-// request previously, but didn't check the "Don't ask again" checkbox.
-        if (shouldProvideRationale) {
-            Log.i(TAG, "Displaying permission rationale to provide additional context.")
-            Snackbar.make(
-                    findViewById(R.id.master),
-                    R.string.permission_rationale,
-                    Snackbar.LENGTH_INDEFINITE)
-                    .setAction(R.string.ok) {
-                        // Request permission
-                        ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                                REQUEST_PERMISSIONS_REQUEST_CODE)
-                    }
-                    .show()
-        } else {
-            Log.i(TAG, "Requesting permission")
-            // Request permission. It's possible this can be auto answered if device policy
-// sets the permission in a given state or the user denied the permission
-// previously and checked "Never ask again".
-            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    REQUEST_PERMISSIONS_REQUEST_CODE)
-        }
-    }
+//    private fun checkPermissions(): Boolean {
+//        return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_FINE_LOCATION)
+//    }
+//
+//    private fun requestPermissions() {
+//        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(this,
+//                Manifest.permission.ACCESS_FINE_LOCATION)
+//        // Provide an additional rationale to the user. This would happen if the user denied the
+//// request previously, but didn't check the "Don't ask again" checkbox.
+//        if (shouldProvideRationale) {
+//            Log.i(TAG, "Displaying permission rationale to provide additional context.")
+//            Snackbar.make(
+//                    findViewById(R.id.master),
+//                    R.string.permission_rationale,
+//                    Snackbar.LENGTH_INDEFINITE)
+//                    .setAction(R.string.ok) {
+//                        // Request permission
+//                        ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+//                                REQUEST_PERMISSIONS_REQUEST_CODE)
+//                    }
+//                    .show()
+//        } else {
+//            Log.i(TAG, "Requesting permission")
+//            // Request permission. It's possible this can be auto answered if device policy
+//// sets the permission in a given state or the user denied the permission
+//// previously and checked "Never ask again".
+//            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+//                    REQUEST_PERMISSIONS_REQUEST_CODE)
+//        }
+//    }
 
     /**
      * Callback received when a permissions request has been completed.
      */
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>,
-                                            grantResults: IntArray) {
-        Log.i(TAG, "onRequestPermissionResult")
-        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
-            if (grantResults.size <= 0) { // If user interaction was interrupted, the permission request is cancelled and you
-// receive empty arrays.
-                Log.i(TAG, "User interaction was cancelled.")
-            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) { // Permission was granted.
-                mService!!.requestLocationUpdates()
-            } else { // Permission denied.
-                // setButtonsState(false)
-                Snackbar.make(
-                        findViewById(R.id.master),
-                        R.string.permission_denied_explanation,
-                        Snackbar.LENGTH_INDEFINITE)
-                        .setAction(R.string.settings) {
-                            // Build intent that displays the App settings screen.
-                            val intent = Intent()
-                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                            val uri = Uri.fromParts("package",
-                                    BuildConfig.APPLICATION_ID, null)
-                            intent.data = uri
-                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                            startActivity(intent)
-                        }
-                        .show()
-            }
-        }
-    }
-
-    /**
-     * Receiver for broadcasts sent by [LocationUpdatesService].
-     */
-    private fun fetchAddress(location: Location) {
-
-
-        if (!Geocoder.isPresent()) {
-            Toast.makeText(this@MainActivity,
-                    "no geoorder",
-                    Toast.LENGTH_LONG).show()
-            return
-        }
-        // Start service  to reflect new location
-        startIntentService(location)
-    }
-
-    inner class MyReceiver : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            var location = intent?.getParcelableExtra<Location>(LocationUpdatesService.EXTRA_LOCATION)
-
-            if (location != null&&fetchFinish) {
-                fetchFinish = false
-                this@MainActivity.fetchAddress(location)
-            }
-        }
-    }
-
-    private fun startIntentService(location : Location) {
-
-        val intent = Intent(this, FetchAddressIntentService::class.java).apply {
-            putExtra(FetchAddressIntentService.Constants.RECEIVER, resultReceiver)
-            putExtra(FetchAddressIntentService.Constants.LOCATION_DATA_EXTRA, location)
-        }
-        startService(intent)
-    }
-
-    internal inner class AddressResultReceiver(handler: Handler) : ResultReceiver(handler) {
-
-        override fun onReceiveResult(resultCode: Int, resultData: Bundle?) {
-
-            // Display the address string
-            // or an error message sent from the intent service.
-            var addressOutput = resultData?.getString(FetchAddressIntentService.Constants.RESULT_DATA_KEY)
-                    ?: ""
-
-            // Show a toast message if an address was found.
-            if (resultCode == FetchAddressIntentService.Constants.SUCCESS_RESULT) {
-                Toast.makeText(this@MainActivity, addressOutput,
-                        Toast.LENGTH_SHORT).show()
-            }
-            fetchFinish = true
-
-        }
-
-    }
+//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>,
+//                                            grantResults: IntArray) {
+//        Log.i(TAG, "onRequestPermissionResult")
+//        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+//            if (grantResults.size <= 0) { // If user interaction was interrupted, the permission request is cancelled and you
+//// receive empty arrays.
+//                Log.i(TAG, "User interaction was cancelled.")
+//            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) { // Permission was granted.
+//                mService!!.requestLocationUpdates()
+//            } else { // Permission denied.
+//                // setButtonsState(false)
+//                Snackbar.make(
+//                        findViewById(R.id.master),
+//                        R.string.permission_denied_explanation,
+//                        Snackbar.LENGTH_INDEFINITE)
+//                        .setAction(R.string.settings) {
+//                            // Build intent that displays the App settings screen.
+//                            val intent = Intent()
+//                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+//                            val uri = Uri.fromParts("package",
+//                                    BuildConfig.APPLICATION_ID, null)
+//                            intent.data = uri
+//                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+//                            startActivity(intent)
+//                        }
+//                        .show()
+//            }
+//        }
+//    }
+//
+//    /**
+//     * Receiver for broadcasts sent by [LocationUpdatesService].
+//     */
+//    private fun fetchAddress(location: Location) {
+//
+//
+//        if (!Geocoder.isPresent()) {
+//            Toast.makeText(this@MainActivity,
+//                    "no geoorder",
+//                    Toast.LENGTH_LONG).show()
+//            return
+//        }
+//        // Start service  to reflect new location
+//        startIntentService(location)
+//    }
+//
+//    inner class MyReceiver : BroadcastReceiver() {
+//        override fun onReceive(context: Context?, intent: Intent?) {
+//            var location = intent?.getParcelableExtra<Location>(LocationUpdatesService.EXTRA_LOCATION)
+//
+//            if (location != null&&fetchFinish) {
+//                fetchFinish = false
+//                this@MainActivity.fetchAddress(location)
+//            }
+//        }
+//    }
+//
+//    private fun startIntentService(location : Location) {
+//
+//        val intent = Intent(this, FetchAddressIntentService::class.java).apply {
+//            putExtra(FetchAddressIntentService.Constants.RECEIVER, resultReceiver)
+//            putExtra(FetchAddressIntentService.Constants.LOCATION_DATA_EXTRA, location)
+//        }
+//        startService(intent)
+//    }
+//
+//    internal inner class AddressResultReceiver(handler: Handler) : ResultReceiver(handler) {
+//
+//        override fun onReceiveResult(resultCode: Int, resultData: Bundle?) {
+//
+//            // Display the address string
+//            // or an error message sent from the intent service.
+//            var addressOutput = resultData?.getString(FetchAddressIntentService.Constants.RESULT_DATA_KEY)
+//                    ?: ""
+//
+//            // Show a toast message if an address was found.
+//            if (resultCode == FetchAddressIntentService.Constants.SUCCESS_RESULT) {
+//                Toast.makeText(this@MainActivity, addressOutput,
+//                        Toast.LENGTH_SHORT).show()
+//            }
+//            fetchFinish = true
+//
+//        }
+//
+//    }
 
 
 
